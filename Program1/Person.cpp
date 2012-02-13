875a4c644605c540cb4d88aa7bc8c6c95f4c9044
Evan Sussman <esussma1@binghamton.edu>
2012-02-12 22:45:05 -0500

    stupid, havent commited in awhile. Hooked up all of the code except the destructors

--- Program1/Person.cpp
+++ Program1/Person.cpp
@@ -4,58 +4,123 @@
 #include <string.h>
 #include <iostream>
 
+void research(Professor *professor, const char * topic)
+{
+   // Must print "<name> does research in <topic>."
+  printf("%s does research in %s\n", professor->person.name, topic);
+}
+
+void graduate_under(Student *student)
+{
+  Under *under = Student_downcast_Under(student);
+  printf("%s graduates in %d years and finds a job in %s or goes to grad school.\n", under->student.person.name, under->year, under->student.major);
+}
+void graduate_grad(Student *student)
+{
+  Grad *grad = Student_downcast_Grad(student);
+    printf("%s graduates with a %s and finds a job in %s.\n", grad->student.person.name, grad->degree, grad->student.major);
+}
+
+
 bool type_check_under(const char* type)
 {
-  if(type == "under")
+  if(strcmp(type, "under") == 0)
     return true;
-  else if (type == "student")
+  else if (strcmp(type, "student") == 0)
     return true;
-  else if (type == "person")
+  else if (strcmp(type , "person") == 0)
+    return true;
+
+  return false;
+}
+bool type_check_grad(const char* type)
+{
+  if(strcmp(type, "grad") == 0)
+    return true;
+  else if (strcmp(type,"student") == 0)
+    return true;
+  else if (strcmp(type,"person") == 0)
+    return true;
+
+  return false;
+}
+bool type_check_professor(const char* type)
+{
+  if(strcmp(type,"professor") == 0)
+    return true;
+  else if (strcmp(type,"person") == 0)
     return true;
   return false;
 }
 Student* Person_downcast_Student(Person* person)
 {
-
+  if(person->type_check("student"))
+  {
+    return (Student *)person;
+  }
+  return NULL;
 }
 Under* Person_downcast_Under(Person* person)
 {
-
+  if(person->type_check("under"))
+  {
+    return (Under *)person;
+  }
+  return NULL;
 }
 Under* Student_downcast_Under(Student* student)
 {
-
+  if(student->person.type_check("under"))
+  {
+    return (Under *)student;
+  }
+  return NULL;
 }
 Grad* Person_downcast_Grad(Person* person)
 {
-
+  if(person->type_check("grad"))
+  {
+    return (Grad *)person;
+  }
+  return NULL;
 }
 Professor* Person_downcast_Professor(Person* person)
 {
-
+  if(person->type_check("professor"))
+  {
+    return (Professor *)person;
+  }
+  return NULL;
 }
 Grad* Student_downcast_Grad(Student* student)
 {
-
+  if(student->person.type_check("grad"))
+  {
+    return (Grad *)student;
+  }
+  return NULL;
 }
 void professor_work(Person *person)
 {
-  //printf("%s studies %s");
-
+  printf("%s teaches.\n", person->name);
 }
 
 void student_work(Person *person)
 {
-  //printf("%s studies %s");
+  //<name> studies <major>
+  Student *student = Person_downcast_Student(person);
+  printf("%s studies %s\n", student->person.name, student->major);
 
 }
 Under* Under_new(char * name, char gender, char * major, int year)
 {
   Under *under = (Under*) malloc(sizeof (Under));
-  under->student.person.name = name;
+  under->student.person.name = strdup(name);
   under->student.person.gender = gender;
+  under->student.person.type_check = &type_check_under;
   under->student.person.work = &student_work;
-  under->student.major = major;
+  under->student.graduate = &graduate_under;
+  under->student.major = strdup(major);
   under->year = year;
 
   return under;
@@ -64,21 +129,25 @@ Under* Under_new(char * name, char gender, char * major, int year)
 Grad* Grad_new(char * name, char gender, char * major, char * degree)
 {
   Grad *grad = (Grad*) malloc (sizeof (Grad));
-  grad->student.person.name = name;
+  grad->student.person.name = strdup(name);
   grad->student.person.gender = gender;
+  grad->student.person.type_check = &type_check_grad;
   grad->student.person.work = &student_work;
-  grad->student.major = major;
-  grad->degree = degree;
+  grad->student.major = strdup(major);
+  grad->student.graduate = &graduate_grad;
+  grad->degree = strdup(degree);
 
   return grad;
 }
 Professor* Professor_new(char * name, char gender, char * office)
 {
   Professor *professor = (Professor*) malloc (sizeof (Professor));
-  professor->person.name = name;
+  professor->person.name = strdup(name);
   professor->person.gender = gender;
+  professor->person.type_check = &type_check_professor;
   professor->person.work = &professor_work;
-  professor->office = office;
+  professor->research = &research;
+  professor->office = strdup(office);
   return professor;
 }
 
