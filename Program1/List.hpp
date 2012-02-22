de18b7296a5bf5a8d047312c259701d85c583415
Evan Sussman <esussma1@binghamton.edu>
2012-02-22 12:26:09 -0500

    got list_new to take in the function pointer by using PersonPtr rather then person*

--- Program1/List.hpp
+++ Program1/List.hpp
@@ -1,13 +1,29 @@
 typedef struct Node_{
 	Node_ *next;
 	Node_ *prev; //maybe
+
+  Person person;
+
 } Node;
 
-typedef struct Iterator_{
+typedef struct List_PersonPtr_Iterator_{
 	Node *mynode;
-} Iterator;
+  Person* (*deref)(List_PersonPtr_Iterator_ *it);
+  List_PersonPtr_Iterator_ (*inc)(List_PersonPtr_Iterator_ *it);
+} List_PersonPtr_Iterator;
 
-typedef struct List_{
+typedef Person *PersonPtr;
+
+typedef struct List_PersonPtr_{
 	Node *head;
   Node *last;
-}List;
+  char *type;
+
+  bool (*PersonPtr_less)(const PersonPtr &p1, const PersonPtr &p2);
+  List_PersonPtr_Iterator (*begin)(List_PersonPtr_ *list);
+  List_PersonPtr_Iterator (*end)(List_PersonPtr_ *list);
+
+}List_PersonPtr;
+
+bool List_PersonPtr_Iterator_equal (List_PersonPtr_Iterator_ first_it, List_PersonPtr_Iterator_ second_it);
+List_PersonPtr* List_PersonPtr_new(bool (*PersonPtr_less)(const PersonPtr &p1, const PersonPtr &p2));
