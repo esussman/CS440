3276647f2a4cc36fcb37067918031bdc02bebe06
Evan Sussman <esussma1@binghamton.edu>
2012-02-29 11:46:56 -0500

    I am the List_PersonPtr implementation, fix my sort function please!

--- Program1/List2.hpp
+++ Program1/List2.hpp
@@ -1,200 +1,223 @@
-#define List_DEFINE(arg)\
-  \
-typedef arg* arg##Ptr;\
-\
-typedef struct Node_##arg##_{\
-  Node_##arg##_* next;\
-  Node_##arg##_* prev;\
-  arg elem;\
-} Node_##arg;\
-\
-typedef struct List_##arg##_Iterator_{\
-  Node_##arg* node;\
-  arg (*deref)(List_##arg##_Iterator_*);\
-  void (*inc)(List_##arg##_Iterator_*);\
-  void (*dec)(List_##arg##_Iterator_*);\
-} List_##arg##_Iterator;\
-\
-typedef struct List_##arg##_{\
-  Node_##arg head;\
-  char type[sizeof("List_" #arg)];\
-  int size;\
-  bool (*compare)(const arg &p1, const arg &p2);\
-  List_##arg##_Iterator (*push_front)(List_##arg##_* , arg);\
-  List_##arg##_Iterator (*push_back)(List_##arg##_* , arg);\
-  void (*sort)(List_##arg##_*);\
-  void (*erase)(List_##arg##_* , List_##arg##_Iterator);\
-  List_##arg##_Iterator (*begin)(List_##arg##_* );\
-  List_##arg##_Iterator (*end)(List_##arg##_* );\
-  void (*delet)(List_##arg##_*);\
-}List_##arg;\
-\
-Node_##arg* Node_##arg##_new(arg elem)\
-{\
-  Node_##arg *n = (Node_##arg*) malloc (sizeof(Node_##arg));\
-  n->next = NULL;\
-  n->prev = NULL;\
-  n->elem = elem;\
-  return n;\
-}\
-arg Iterator_##arg##_deref(List_##arg##_Iterator_* it)\
-{\
-  Node_##arg* p = it->node;\
-  return p->elem;\
-}\
-bool List_##arg##_Iterator_equal(List_##arg##_Iterator it1, List_##arg##_Iterator it2)\
-{\
-  if(it1.node == it2.node)\
-    return true;\
-  else\
-    return false;\
-}\
-\
-\
-void List_##arg##_it_inc(List_##arg##_Iterator* it) {\
-    it->node = it->node->next;\
-}\
-void List_##arg##_it_dec(List_##arg##_Iterator* it) {\
-  it->node = it->node->prev;\
-}\
-\
-List_##arg##_Iterator List_##arg##_it_ctor(Node_##arg* node)\
-{\
-  List_##arg##_Iterator it;\
-  it.node = node;\
-  it.deref = *Iterator_##arg##_deref;\
-  it.inc = List_##arg##_it_inc;\
-  it.dec = List_##arg##_it_dec;\
-  return it;\
-}\
-void insert_before(Node_##arg* pos, Node_##arg* n)\
-{\
-  n->next = pos;\
-  n->prev = pos->prev;\
-  pos->prev->next = n;\
-  pos->prev = n;\
-}\
-List_##arg##_Iterator push_front(List_##arg* list, arg elem)\
-{\
-  Node_##arg *n = Node_##arg##_new(elem);\
-  insert_before(list->head.next, n);\
-  List_##arg##_Iterator it = List_##arg##_it_ctor(list->head.next);\
-  list->size = list->size + 1;\
-  return it;\
-}\
-List_##arg##_Iterator push_back(List_##arg* list, arg elem)\
-{\
-  Node_##arg *n = Node_##arg##_new(elem);\
-  insert_before(&list->head, n);\
-  List_##arg##_Iterator it = List_##arg##_it_ctor(list->head.prev);\
-  list->size = list->size + 1;\
-  return it;\
-}\
-\
-Node_##arg* node_sort(Node_##arg* list, bool (*compare)(const arg &p1, const arg &p2))\
-{\
-  int listSize=1,numMerges,leftSize,rightSize;\
-    Node_##arg *tail,*left,*right,*next;\
-    if (!list || !list->next) return list;\
-    do {\
-            numMerges=0,left=list;tail=list=0;\
-            while (left) {\
-                        numMerges++,right=left,leftSize=0,rightSize=listSize;\
-                        while (right && leftSize<listSize) leftSize++,right=right->next;\
-                        while (leftSize>0 || (rightSize>0 && right)) {\
-                                        if (!leftSize)                  {\
-                                                          next=right;\
-                                                          right=right->next;\
-                                                          rightSize--;\
-                                                        }\
-                                        else if (!rightSize || !right)\
-                                        {\
-                                                          next=left;\
-                                                          left=left->next;\
-                                                          leftSize--;\
-                                                        }\
-                                        else if (compare(left->elem,right->elem))\
-                                        {\
-                                                          next=left;\
-                                                          left=left->next;\
-                                                          leftSize--;\
-                                                        }\
-                                        else\
-                                        {\
-                                                          next=right;\
-                                                          right=right->next;\
-                                                          rightSize--;\
-                                                        }\
-                                        if (tail)\
-                                          tail->next=next;\
-                                        else list=next;\
-                                        next->prev=tail;\
-                                        tail=next;\
-                                    }\
-                        left=right;\
-                    }\
-            tail->next=0,listSize<<=1;\
-        } while (numMerges>1);\
-    return list;\
-}\
-\
-void sort(List_##arg* list)\
-{\
-  list->head.prev->next = NULL;\
-  Node_##arg* reattach = node_sort(list->head.next, list->compare);\
-  reattach->prev = &list->head;\
-  while(reattach->next != NULL)\
-  {\
-      reattach = reattach->next;\
-    }\
-  reattach->next = &list->head;\
-  list->head.prev = reattach;\
-}\
-\
-void erase(List_##arg##_* list, List_##arg##_Iterator it)\
-{\
-  Node_##arg *n = it.node;\
-  n->next->prev = n->prev;\
-  n->prev->next = n->next;\
-  list->size = list->size - 1;\
-  free(n);\
-}\
-void List_##arg##_delet(List_##arg* list)\
-{\
-  if(list->size == 0)\
-  {\
-      /*free(&list->head);*/\
-      free(list);\
-    }\
-  else\
-  {\
-      List_##arg##_Iterator it = list->begin(list);\
-      list->erase(list, it);\
-      List_##arg##_delet(list);\
-    }\
-}\
-\
-List_##arg##_Iterator List_##arg##_begin(List_##arg* list) {\
-    return List_##arg##_it_ctor(list->head.next);\
-}\
-List_##arg##_Iterator List_##arg##_end(List_##arg* list) {\
-    return List_##arg##_it_ctor(&list->head);\
-}\
-List_##arg* List_##arg##_new(bool (*compare)(const arg &p1, const arg &p2))\
-{\
-  List_##arg *List = (List_##arg*)malloc(sizeof(List_##arg));\
-  List->head.next = &List->head;\
-  List->head.prev = &List->head;\
-  List->compare = compare;\
-  List->size = 0;\
-  strcpy(List->type, "List_" #arg);\
-  List->push_front = push_front;\
-  List->push_back = push_back;\
-  List->sort = sort;\
-  List->erase = erase;\
-  List->begin = List_##arg##_begin;\
-  List->end = List_##arg##_end;\
-  List->delet = List_##arg##_delet;\
-  return List;\
+typedef Person* PersonPtr;
+
+typedef struct Node_{
+	Node_ *next;
+  Node_ *prev;
+  Person *person;
+} Node;
+
+typedef struct List_PersonPtr_Iterator_{
+	Node *node;
+  PersonPtr& (*deref)(List_PersonPtr_Iterator_*);
+  void (*inc)(List_PersonPtr_Iterator_*);
+  void (*dec)(List_PersonPtr_Iterator_*);
+} List_PersonPtr_Iterator;
+
+typedef struct List_PersonPtr_{
+  Node head;
+  char type[sizeof("List_PersonPtr")];
+  int size;
+  bool (*compare)(const PersonPtr &p1, const PersonPtr &p2);
+  List_PersonPtr_Iterator (*push_front)(List_PersonPtr_* , Person*);
+  List_PersonPtr_Iterator (*push_back)(List_PersonPtr_* , Person*);
+
+  void (*sort)(List_PersonPtr_*);
+  void (*erase)(List_PersonPtr_* , List_PersonPtr_Iterator);
+  List_PersonPtr_Iterator (*begin)(List_PersonPtr_* );
+  List_PersonPtr_Iterator (*end)(List_PersonPtr_* );
+  void (*delet)(List_PersonPtr_*);
+
+}List_PersonPtr;
+
+Node* Node_new(Person* person)
+{
+  Node *n = (Node*) malloc (sizeof(Node));
+  n->next = NULL;
+  n->prev = NULL;
+  n->person = person;
+  return n;
+}
+Person* &Iterator_PersonPtr_deref(List_PersonPtr_Iterator_* it)
+{
+  Node *p = it->node;
+  return p->person;
+}
+bool List_PersonPtr_Iterator_equal (List_PersonPtr_Iterator it1, List_PersonPtr_Iterator it2)
+{
+  if(it1.node == it2.node)
+    return true;
+  else
+    return false;
 }
 
+void List_PersonPtr_it_inc(List_PersonPtr_Iterator* it) {
+    it->node = it->node->next;
+}
+void List_PersonPtr_it_dec(List_PersonPtr_Iterator* it) {
+  it->node = it->node->prev;
+}
+
+List_PersonPtr_Iterator List_PersonPtr_it_ctor(Node* node)
+{
+  List_PersonPtr_Iterator it;
+  it.node = node;
+  it.deref = *Iterator_PersonPtr_deref;
+  it.inc = List_PersonPtr_it_inc;
+  it.dec = List_PersonPtr_it_dec;
+  return it;
+}
+void insert_before(Node* pos, Node* n)
+{
+  n->next = pos;
+  n->prev = pos->prev;
+  pos->prev->next = n;
+  pos->prev = n;
+}
+List_PersonPtr_Iterator push_front(List_PersonPtr_* list, Person* person)
+{
+  Node *n = Node_new(person);
+  insert_before(list->head.next, n);
+  List_PersonPtr_Iterator it = List_PersonPtr_it_ctor(list->head.next);
+  list->size = list->size + 1;
+  return it;
+}
+List_PersonPtr_Iterator push_back(List_PersonPtr_* list, Person* person)
+{
+  Node *n = Node_new(person);
+  insert_before(&list->head, n);
+  List_PersonPtr_Iterator it = List_PersonPtr_it_ctor(list->head.prev);
+  list->size = list->size + 1;
+  return it;
+}
+
+List_PersonPtr_Iterator SortedMerge(List_PersonPtr *list, List_PersonPtr_Iterator a, List_PersonPtr_Iterator b)
+{
+  // A: begin, slow
+  // B: slow, end
+  if(a.node == NULL)
+    return b;
+  else if(b.node == NULL)
+    return a;
+
+  List_PersonPtr_Iterator result;
+  if(list->compare(a.deref(&a), b.deref(&b)))
+  {
+    List_PersonPtr_Iterator temp = a;
+    temp.inc(&temp);
+    result = a;
+    b = SortedMerge(list, temp, b);
+    result.node->next = b.node;
+    b.node->prev = result.node;
+  }
+  else
+  {
+    List_PersonPtr_Iterator temp = b;
+    temp.inc(&temp);
+    result = b;
+    a = SortedMerge(list, a , temp);
+    result.node->next = a.node;
+    a.node->prev = result.node;
+  }
+
+  return result;
+}
+List_PersonPtr_Iterator node_sort(List_PersonPtr *list, List_PersonPtr_Iterator begin, List_PersonPtr_Iterator end)
+{
+  List_PersonPtr_Iterator res1;
+  List_PersonPtr_Iterator res2;
+
+  if(begin.node == end.node)
+  {
+    begin.node->next = NULL;
+    return begin;
+  }
+  else
+  {
+    List_PersonPtr_Iterator slow = begin;
+    List_PersonPtr_Iterator fast = begin;
+    fast.inc(&fast);
+    while(!List_PersonPtr_Iterator_equal(fast,end))
+    {
+      fast.inc(&fast);
+      if(!List_PersonPtr_Iterator_equal(fast,end))
+      {
+        fast.inc(&fast);
+        slow.inc(&slow);
+      }
+    }
+    List_PersonPtr_Iterator slow_next = slow;
+    slow_next.inc(&slow_next);
+    slow.node->next = NULL;
+    res1 = node_sort(list, begin, slow);
+    res2 = node_sort(list, slow_next, end);
+  }
+  return SortedMerge(list, res1, res2);
+}
+void reinsertPrevPointers(List_PersonPtr* list)
+{
+  Node* fast = list->head.next;
+  Node* slow = &list->head;
+  while(fast->next != NULL)
+  {
+    fast->prev = slow;
+    slow = slow->next;
+    fast = fast->next;
+  }
+  fast->next = &list->head;
+  list->head.prev = fast;
+}
+void sort(List_PersonPtr* list)
+{
+  List_PersonPtr_Iterator end = list->end(list);
+  end.dec(&end);
+  List_PersonPtr_Iterator head = node_sort(list, list->begin(list), end);
+  list->head.next = head.node;
+  reinsertPrevPointers(list);
+}
+
+void erase(List_PersonPtr_* list, List_PersonPtr_Iterator it)
+{
+  Node *n = it.node;
+  n->next->prev = n->prev;
+  n->prev->next = n->next;
+  list->size = list-> size - 1;
+  free(n);
+}
+void List_PersonPtr_delet(List_PersonPtr* list)
+{
+  if(list->size == 0)
+  {
+    free(list);
+  }
+  else
+  {
+    List_PersonPtr_Iterator it = list->begin(list);
+    list->erase(list, it);
+    List_PersonPtr_delet(list);
+  }
+}
+
+List_PersonPtr_Iterator List_PersonPtr_begin(List_PersonPtr* list) {
+    return List_PersonPtr_it_ctor(list->head.next);
+}
+List_PersonPtr_Iterator List_PersonPtr_end(List_PersonPtr* list) {
+    return List_PersonPtr_it_ctor(&list->head);
+}
+List_PersonPtr* List_PersonPtr_new(bool (*compare)(const PersonPtr &p1, const PersonPtr &p2))
+{
+  List_PersonPtr *List = (List_PersonPtr*)malloc(sizeof(List_PersonPtr));
+  List->head.next = &List->head;
+  List->head.prev = &List->head;
+  List->head.person = NULL;
+  strcpy(List->type, "List_PersonPtr");
+  List->compare = compare;
+  List->size = 0;
+  List->push_front = push_front;
+  List->push_back = push_back;
+  List->sort = sort;
+  List->erase = erase;
+  List->begin = List_PersonPtr_begin;
+  List->end = List_PersonPtr_end;
+  List->delet = List_PersonPtr_delet;
+  return List;
+}
