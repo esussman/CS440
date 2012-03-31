fa479d098e7b40bc40d2cf5e915f97c96ab2e556
Evan Sussman <esussma1@binghamton.edu>
2012-03-31 10:46:37 -0400

    removed the accept method I added, professor was wrong you dont need to visit text with accept

--- Program2/Text.hpp
+++ Program2/Text.hpp
@@ -3,15 +3,16 @@
 
 #include "Node.hpp"
 #include "String.hpp"
-
 namespace xml {
 
   class Text : public Node {
+    friend class Parser;
     public:
       Text();
       const String& str() const;
       static bool is_Text(const Node*);
       static const Text *to_Text(const Node *);
+      //void accept(Visitor *) const;
     private:
       String *contents;
   };
