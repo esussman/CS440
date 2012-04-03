600e4944bc3e70a4392268be1751f267431da0be
Evan Sussman <esussma1@binghamton.edu>
2012-04-03 12:49:20 -0400

    added stack for namespaces

--- Program2/Parser.hpp
+++ Program2/Parser.hpp
@@ -4,6 +4,7 @@
 #include "Element.hpp"
 #include "Text.hpp"
 #include <stack>
+#include <map>
 
 namespace xml{
 
@@ -15,12 +16,14 @@ namespace xml{
       void resetTempString(const char*);
       bool isValidNameChar(const char);
       bool isValidTextChar(const char);
+      bool isValidAddressChar(const char);
       void saveText(Text*);
       void saveElement(Element*);
     private:
       Element *root;
       String *tempString;
       std::stack<Element *> nodeStack;
+      std::stack<std::map<const String, String>> nmspaceStack;
       enum {
         start = 0,
         type_of_tag,
@@ -28,9 +31,15 @@ namespace xml{
         must_be_name,
         close_name_or_namespace,
         close_must_be_name,
+        def_namespace_name,
+        namespace_quote,
+        namespace_definition,
+        end_namespace_definition_ws,
+        check_xmlns,
         inside_body,
         inside_text,
-        tag_name_clear_ws
+        tag_name_clear_ws,
+        close_tag_name_clear_ws
       } state;
   };
 
